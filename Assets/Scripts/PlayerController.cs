2ada80c7f5097f2dc12fdaa59f9ac107561ae4ca
Soly-M-27 <solymar227@icloud.com>
2022-08-16 22:29:21 -0400

    As soon as player collides with Goal, player wins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,5 +52,10 @@ public class PlayerController : MonoBehaviour
             health--;
             Debug.Log($"Health: {health}");
         }
+
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
     }
 }
