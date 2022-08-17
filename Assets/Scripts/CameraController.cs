a881566b0988654fed8c431be282584c4b496fae
Soly-M-27 <solymar227@icloud.com>
2022-08-16 21:28:27 -0400

    Camera followes player based on player objects transforming position

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset = new Vector3(22, 26, 7);
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");   
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;        
     }
 }
